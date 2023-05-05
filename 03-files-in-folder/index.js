eb6c7309aa2f7fc34e52d849559bc3dfc15a7e05
Ivan G <mr.zero@inbox.ru>
2023-05-05 17:56:12 +0300

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+
+const { readdir } = require('node:fs/promises');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+async function scanFolder(folderPath) {
+  try {
+    const files = await readdir(folderPath, { withFileTypes: true });
+    for (const file of files) {
+      fs.stat(`${folderPath}/${file.name}`, (err, stats) => {
+        if (stats.isFile()) {
+          // console.log(stats.size);
+          console.log(`${path.basename(file.name, path.extname(file.name))} - ${path.extname(file.name).slice(1)} - ${stats.size}b`);
+        }
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+scanFolder(folderPath);
